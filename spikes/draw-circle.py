38b03d5febf8fa0c5860ec25eb5e1b6b85a29054
Diego Alfonso <diego.alfonso2@gmail.com>
2021-02-01 23:26:14 -0300

    Algunas nodificaciones de estilo en el ejemplo del circulo y uso show() en vez de save() para mostrar la imagen en lugar de persistirla

--- spikes/draw-circle.py
+++ spikes/draw-circle.py
@@ -1,6 +1,9 @@
 import math
 from PIL import Image, ImageDraw
 
+# size of each grid cell 
+PASO = 25
+
 # size of image
 canvas = (400, 300)
 
@@ -16,27 +19,23 @@ frames = [(250, 250, 5, 5)]
 circle = Image.new('RGBA', canvas, (255, 255, 255, 255))
 draw_circle = ImageDraw.Draw(circle)
 
-# draw rectangles
+# draw circle inside rectangles
 for frame in frames:
     x1, y1 = frame[2], frame[3]
     x2, y2 = frame[2] + frame[0], frame[3] + frame[1]
-    paso = 25
     draw_circle.arc([x1, y1, x2, y2], 0, 360, fill=(0, 0, 0, 255), width=3)
     radius = (x2 - x1) / 2.0
-    for d in range(paso, x2 - x1, paso):
+    for d in range(PASO, x2 - x1, PASO):
       theta = math.asin((radius - d)/ radius)
       delta = radius - math.cos(theta) * radius
       startline = delta
       endline = (x2 - x1) - delta
       draw_circle.line([(x1 + d, y1 + startline), (x1 + d, y1 + endline)], fill=(170, 170, 170, 255), width=1)
       draw_circle.line([(x1 + startline, y1 + d), (x1 + endline, y1 + d)], fill=(170, 170, 170, 255), width=1)
-      #draw_circle.line([(x1, y), (x2, y)], fill=(170, 170, 170, 255), width=1)
-    #draw_circle.rectangle([x1, y1, x2, y2], outline=(170, 170, 170, 255), width=3)
-
-
 
 # make thumbnail
-circle.thumbnail(thumb)
+#circle.thumbnail(thumb)
 
 # save image
-circle.save('circle.png')
+#circle.save('circle.png')
+circle.show()
