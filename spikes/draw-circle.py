09cb566c5214f8517d7c7b449bafba808bc19598
Diego Alfonso <diego.alfonso2@gmail.com>
2021-02-02 01:08:45 -0300

    Metodo is_fully_contained_in_circle para detectar si la celda debe ser pintada entera o en parte

--- spikes/draw-circle.py
+++ spikes/draw-circle.py
@@ -19,8 +19,23 @@ frames = [(250, 250, 5, 5)]
 circle = Image.new('RGBA', canvas, (255, 255, 255, 255))
 draw_circle = ImageDraw.Draw(circle)
 
-def fill_cell(draw, row, column, x1, y1):
-  draw.rectangle([(x1 + PASO * row, y1 + PASO * column), (x1 + PASO * (row + 1), y1 + PASO * (column + 1))], fill=(0, 255, 0, 255))
+def fill_cell(draw, row, column, x1, y1, color=(0,255,0,255)):
+  draw.rectangle([(x1 + PASO * row, y1 + PASO * column), (x1 + PASO * (row + 1), y1 + PASO * (column + 1))], fill=color)
+
+def is_fully_contained_in_circle(radius, row, column):
+  x1_cell, y1_cell = column * PASO, row * PASO
+  x2_cell, y2_cell = x1_cell + PASO, y1_cell + PASO
+  def excedes_radius(x, y):
+    return math.sqrt((radius - x) ** 2 + (radius - y) ** 2) > radius
+  if x1_cell < radius and y1_cell < radius and excedes_radius(x1_cell, y1_cell):
+    return False
+  if x1_cell < radius and y2_cell > radius and excedes_radius(x1_cell, y2_cell):
+    return False
+  if x2_cell > radius and y1_cell < radius and excedes_radius(x2_cell, y1_cell):
+    return False
+  if x2_cell > radius and y2_cell > radius and excedes_radius(x2_cell, y2_cell):
+    return False
+  return True
 
 # draw circle inside rectangles
 for frame in frames:
@@ -35,7 +50,11 @@ for frame in frames:
       endline = (x2 - x1) - delta
       draw_circle.line([(x1 + d, y1 + startline), (x1 + d, y1 + endline)], fill=(170, 170, 170, 255), width=1)
       draw_circle.line([(x1 + startline, y1 + d), (x1 + endline, y1 + d)], fill=(170, 170, 170, 255), width=1)
-    fill_cell(draw_circle, 2,2,x1,y1)
+    for i in range(0, math.ceil((x2 - x1) / float(PASO))):
+      for j in range(0, math.ceil((x2 - x1) / float(PASO))):
+        if is_fully_contained_in_circle(radius, i, j):
+          fill_cell(draw_circle, i, j, x1, y1, color=(255,0,0,255))
+    fill_cell(draw_circle, 2, 2, x1, y1)
 
 # make thumbnail
 #circle.thumbnail(thumb)
