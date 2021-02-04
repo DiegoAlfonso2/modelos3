6f8b09a81afa2f85d2b0193156fc8a5f7f087b21
Diego Alfonso <diego.alfonso2@gmail.com>
2021-02-04 04:05:19 -0300

    Agrego ejemplo mas complejo de grafico de maceta con varias acciones

--- tests/test_graficador_soluciones.py
+++ tests/test_graficador_soluciones.py
@@ -16,3 +16,15 @@ class TestGraficadorSoluciones():
     planta = Planta('Lechuga', (0, 255, 0, 255), [2, 2, 5, 5, 10, 10, 10])
     accion = Accion(planta, maceta, (20, 20), 2)
     graficador.graficar_maceta(maceta, [accion])
+
+  def test_graficar_varias_acciones(self):
+    maceta = Maceta('Jardinera3', 60, 60)
+    graficador = GraficadorSoluciones([maceta])
+    lechuga = Planta('Lechuga', (0, 255, 0, 128), [2, 2, 5, 5, 10, 10, 10])
+    zanahoria = Planta('Zanahoria', (255, 128, 44, 128), [2, 2, 2, 2, 2, 2, 2, 2])
+    repollo = Planta('Repollo', (216, 215, 159), [2, 3, 4, 5, 6, 6])
+    accion = Accion(lechuga, maceta, (20, 20), 2)
+    accion2 = Accion(zanahoria, maceta, (15, 15), 1)
+    accion3 = Accion(repollo, maceta, (40, 50), 4)
+    graficador.graficar_maceta(maceta, [accion, accion3, accion2])
+    
