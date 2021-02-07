8040f71e159e0d944fe88c8152770481355dac76
Diego Alfonso <diego.alfonso2@gmail.com>
2021-02-06 21:17:57 -0300

    Agrego penalizacion en la funcion de fitness

--- model/funcion_fitness.py
+++ model/funcion_fitness.py
@@ -1,6 +1,6 @@
 class FuncionFitness():
   def __init__(self):
-    penalizaciones = []
+    self.penalizaciones = []
   def calcular_fitness_de_solucion(self, solucion):
     if not solucion.es_solucion_factible():
       return 0
@@ -8,4 +8,8 @@ class FuncionFitness():
     for accion in solucion.acciones:
       planta = accion.planta
       fitness += planta.produccion_por_planta * planta.precio
+    for penalizacion in self.penalizaciones:
+      fitness = penalizacion(fitness, solucion)
     return fitness
+  def agregar_penalizacion(self, penalizacion):
+    self.penalizaciones.append(penalizacion)
--- tests/test_funcion_fitness.py
+++ tests/test_funcion_fitness.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from model.funcion_fitness import FuncionFitness
 from model.solucion import Solucion
 from model.planta import Planta
@@ -50,3 +51,25 @@ class TestFuncionFitness():
     sol.agregar_accion(accion1)
     sol.agregar_accion(accion2)
     assert fitness.calcular_fitness_de_solucion(sol) == 0
+
+  def penalizacion_porcentaje_minimo_planta(self, planta, porcentaje, porc_penalizacion):
+    def penalizacion(fitness, solucion):
+      peso_total_cosecha = reduce(lambda x,y: x+y, map(lambda accion: accion.planta.produccion_por_planta, solucion.acciones))
+      peso_planta = reduce(lambda x,y: x+y, map(lambda accion: accion.planta.produccion_por_planta, filter(lambda accion: accion.planta == planta, solucion.acciones)),0)
+      return fitness if float(peso_planta) / peso_total_cosecha >= porcentaje else fitness * porc_penalizacion
+    return penalizacion
+
+  def test_fitness_penalizacion(self):
+    fitness = FuncionFitness()
+    sol = Solucion()
+    lechuga = Planta('Lechuga', (0, 255, 0, 255), [2, 2, 4, 4, 6, 6], 0.25, 180)
+    zanahoria = Planta('Zanahoria', (255, 0, 0, 255), [2, 2, 2, 2, 2], 0.05, 45)
+    jardinera = Maceta('Jardinera', 30, 60)
+    # Si la cantidad de zanahoria no es al menos la mitad en kg de la cosecha total, bajo el funcional un 40%
+    penalizacion = self.penalizacion_porcentaje_minimo_planta(zanahoria, 0.5, 0.6)
+    accion1 = Accion(lechuga, jardinera, (10, 10), 1)
+    accion2 = Accion(lechuga, jardinera, (10, 30), 1)
+    sol.agregar_accion(accion1)
+    sol.agregar_accion(accion2)
+    fitness.agregar_penalizacion(penalizacion)
+    assert fitness.calcular_fitness_de_solucion(sol) == 54
