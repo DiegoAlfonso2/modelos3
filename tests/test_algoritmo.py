9dc63c88218ccf5159ff51121b893aa455b51a39
Diego Alfonso <diego.alfonso2@gmail.com>
2021-02-08 01:17:57 -0300

    Agrego una prueba del algoritmo con dos macetas y dos plantas

--- tests/test_algoritmo.py
+++ tests/test_algoritmo.py
@@ -14,12 +14,26 @@ class TestAlgoritmo():
     # Si la cantidad de zanahoria no es al menos la mitad en kg de la cosecha total, bajo el funcional un 40%
     penalizacion = self.penalizacion_porcentaje_minimo_planta(zanahoria, 0.5, 0.6)
     fitness.agregar_penalizacion(penalizacion)
-    algoritmo = Algoritmo(20, fitness)
+    algoritmo = Algoritmo(8, fitness)
     solucion, valor = algoritmo.obtener_solucion_optima([lechuga, zanahoria], [jardinera])
     print('El fitness de la mejor solucion obtenida es', valor)
     graficador = GraficadorSoluciones([jardinera])
     graficador.graficar_solucion(solucion)
 
+  def test_algoritmo_dos_macetas_dos_plantas(self):
+    fitness = FuncionFitness()
+    lechuga = Planta('Lechuga', (0, 255, 0, 255), [2, 2, 4, 4, 6, 6], 0.25, 180)
+    zanahoria = Planta('Zanahoria', (255, 0, 0, 255), [2, 2, 2, 2, 2], 0.05, 45)
+    jardinera = Maceta('Jardinera', 30, 60)
+    grande = Maceta('MacetaGrande', 60, 60)
+    # Si la cantidad de zanahoria no es al menos la mitad en kg de la cosecha total, bajo el funcional un 40%
+    penalizacion = self.penalizacion_porcentaje_minimo_planta(zanahoria, 0.5, 0.6)
+    fitness.agregar_penalizacion(penalizacion)
+    algoritmo = Algoritmo(8, fitness)
+    solucion, valor = algoritmo.obtener_solucion_optima([lechuga, zanahoria], [jardinera, grande])
+    print('El fitness de la mejor solucion obtenida es', valor)
+    graficador = GraficadorSoluciones([jardinera])
+    graficador.graficar_solucion(solucion)
   #TODO metodo repetido
   def penalizacion_porcentaje_minimo_planta(self, planta, porcentaje, porc_penalizacion):
     def penalizacion(fitness, solucion):
